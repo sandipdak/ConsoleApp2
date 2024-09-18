e31188ef40c1d058f9cf644b34009e5c0fdb3145
OWN-PC <OWN-PC@DESKTOP-DMEFU7F>
2024-09-18 22:11:31 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string str="Hello1";
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
